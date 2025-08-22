c45b0f45a11314108547c4db71edd34d12dc7d08
Paygg <panthakranpeng@gmail.com>
2025-08-22 12:18:34 +0700

    update1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h1>Panthakran Pengpinij ID 67711727</h1>
+    <h2>Panthakran Pengpinij ID 67711727</h2>
+    <h3>Information Technology</h3>
   </div>
 </template>
